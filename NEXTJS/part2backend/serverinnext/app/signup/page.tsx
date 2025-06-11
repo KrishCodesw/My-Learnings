c09d7467605d598a000d4a70723c63f3be748997
KrishCodesw <krishjain.w@gmail.com>
2025-06-12 00:57:40 +0530

    Prisma Singleton client for dev mode for hot reloading avoiding

--- NEXTJS/part2backend/serverinnext/app/signup/page.tsx
+++ NEXTJS/part2backend/serverinnext/app/signup/page.tsx
@@ -2,53 +2,50 @@
 
 import axios from "axios";
 import { ChangeEventHandler, useState } from "react";
+import { useRouter } from "next/navigation";
 
 export default function Signup() {
   const [username, setUsername] = useState("");
   const [password, setPassword] = useState("");
-
+  const router = useRouter();
   return (
     <div className="h-screen flex justify-center flex-col">
       <div className="flex justify-center">
-        <a
-          href="#"
-          className="block max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow hover:bg-gray-100 "
-        >
-          <div>
-            <div className="px-10">
-              <div className="text-3xl font-extrabold">Sign up</div>
-            </div>
-            <div className="pt-2">
-              <LabelledInput
-                onChange={(e) => {
-                  setUsername(e.target.value);
-                }}
-                label="Username"
-                placeholder="harkirat@gmail.com"
-              />
-              <LabelledInput
-                onChange={(e) => {
-                  setPassword(e.target.value);
-                }}
-                label="Password"
-                type={"password"}
-                placeholder="123456"
-              />
-              <button
-                type="button"
-                className="mt-8 w-full text-white bg-gray-800 focus:ring-4 focus:ring-gray-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2"
-                onClick={() => {
-                  axios.post("http://localhost:3000/api/v1/signup", {
-                    username,
-                    password,
-                  });
-                }}
-              >
-                Sign in
-              </button>
-            </div>
+        <div>
+          <div className="px-10">
+            <div className="text-3xl font-extrabold">Sign up</div>
           </div>
-        </a>
+          <div className="pt-2">
+            <LabelledInput
+              onChange={(e) => {
+                setUsername(e.target.value);
+              }}
+              label="Username"
+              placeholder="harkirat@gmail.com"
+            />
+            <LabelledInput
+              onChange={(e) => {
+                setPassword(e.target.value);
+              }}
+              label="Password"
+              type={"password"}
+              placeholder="123456"
+            />
+            <button
+              type="button"
+              className="mt-8 w-full text-white bg-gray-800 focus:ring-4 focus:ring-gray-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2"
+              onClick={async () => {
+                await axios.post("http://localhost:3000/api/v1/signup", {
+                  username,
+                  password,
+                });
+                router.push("/signin");
+              }}
+            >
+              Sign in
+            </button>
+          </div>
+        </div>
       </div>
     </div>
   );
