2c14c2277f766d68e0f95f908f8396ff42355918
KrishCodesw <krishjain.w@gmail.com>
2025-06-30 15:18:07 +0530

    Monorepos

---
+++ Monorepos/apps/web/app/admin/page.tsx
@@ -0,0 +1,12 @@
+import { Admin } from "@repo/ui/admin";
+import { Navbar } from "@repo/ui/navbar";
+const page = () => {
+  return (
+    <div>
+      <Admin />
+      <Navbar />
+    </div>
+  );
+};
+
+export default page;
