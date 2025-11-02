bd261d89cf0a862b5297b6fc0e645371bd1002fa
KrishCodesw <krishjain.w@gmail.com>
2025-11-02 17:29:21 +0530

    Created endpoints which expose a request time metric

--- Prometheus/index.ts
+++ Prometheus/index.ts
@@ -1,1 +1,27 @@
-console.log("Hello via Bun!");
+import express, { json } from 'express'
+import { middleware } from './middleware';
+
+const app =express();
+
+app.use(json());
+app.use(middleware)
+
+
+
+
+app.get("/cpu",(req,res)=>{
+    for(let i=0;i<100000;i++){
+        Math.random();
+    }
+    res.json({
+        message:"cpu"
+    })
+})
+app.get("/user",(req,res)=>{
+    
+    res.json({
+        message:"user"
+    })
+})
+
+app.listen(3000);
