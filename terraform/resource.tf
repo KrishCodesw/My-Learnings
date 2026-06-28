e24569e6c454de4ecce08fcef3ada2a12c4f2596
KrishCodesw <krishjain.w@gmail.com>
2026-06-28 12:00:01 +0530

    resource

--- terraform/resource.tf
+++ terraform/resource.tf
@@ -1,3 +1,5 @@
-resource "local_file" "name" {
-  
+resource "local_file" "tf_ex1" {
+  content = "foo!"
+  #   filename = "terraform\terraform" relative path
+  filename = "${path.module}/example1.txt" //modular
 }
