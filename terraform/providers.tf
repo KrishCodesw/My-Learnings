e516c41e086b97e306628dead26fae36556e395d
KrishCodesw <krishjain.w@gmail.com>
2026-06-28 11:48:53 +0530

    seperating

--- terraform/providers.tf
+++ terraform/providers.tf
@@ -4,21 +4,21 @@ terraform {
     local = {
       source = "hashicorp/local"
       # Setting the provider version is a strongly recommended practice
-      version = "6.52.0"
+      # version = "6.52.0"
     }
 
-    aws = {
-      source  = "hashicorp/aws"
-      version = "6.52.0"
-    }
+    # aws = {
+    #   source  = "hashicorp/aws"
+    #   version = "6.52.0"
+    # }
   }
   # Provider functions require Terraform 1.8 and later.
   required_version = ">= 1.8.0"
 }
 
-provider "local" {
+# provider "local" {
 
-}
+# }
 
 output "example_output" {
   value = provider::local::direxists("${path.module}/example-directory")
