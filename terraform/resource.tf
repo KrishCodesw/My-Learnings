74f5b52c5f1f217dcbd3bc05989aad44bb34a265
KrishCodesw <krishjain.w@gmail.com>
2026-06-29 13:26:55 +0530

    up

--- terraform/resource.tf
+++ terraform/resource.tf
@@ -2,6 +2,7 @@ resource "local_file" "tf_ex1" {
   content = "foo! mantar !"
   #   filename = "terraform\terraform" relative path
   filename = "${path.module}/example1.txt" //modular
+  filename = "${path.module}/example1.txt" //modular
 
   // For every resource - > there are some attributes that are compulsary to be defined 
 }
