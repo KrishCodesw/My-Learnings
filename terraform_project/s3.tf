fd603a3d7105c7eb1212a387f615eb0389ed29d8
KrishCodesw <krishjain.w@gmail.com>
2026-06-30 20:06:41 +0530

    s3

--- terraform_project/s3.tf
+++ terraform_project/s3.tf
@@ -1,8 +1,26 @@
 resource "aws_s3_bucket" "tf_s3_bucket" {
-  bucket = "my-tf-test-bucket"
+  bucket = "my-tf-test-bucket for nodejs-app-static-files-bkt"
 
   tags = {
-    Name        = "My bucket"
+    Name        = "My bucket for Nodejs"
     Environment = "Dev"
   }
+}
+
+
+resource "aws_s3_object" "object" {
+  bucket   = aws_s3_bucket.tf_s3_bucket
+  key      = "images/${each.key}"
+  source   = "nodejs-mysql/public/images/${each.key}"
+  for_each = fileset("nodejs-mysql/public/images", "**") # get all files in the images directory
+
+  # The filemd5() function is available in Terraform 0.11.12 and later
+  # For Terraform 0.11.11 and earlier, use the md5() function and the file() function:
+  # etag = "${md5(file("path/to/file"))}"
+  etag = filemd5("path/to/file")
+}
+
+
+output "s3_bucket_id" {
+  value = aws_s3_bucket.tf_s3_bucket.id
 }
