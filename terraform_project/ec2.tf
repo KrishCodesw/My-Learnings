43df1465c127c609b586cfa0c7576208df6ab621
KrishCodesw <krishjain.w@gmail.com>
2026-06-30 21:23:42 +0530

    ec2

--- terraform_project/ec2.tf
+++ terraform_project/ec2.tf
@@ -35,7 +35,7 @@ resource "aws_instance" "tf_server" {
                                 sudo apt install -y nodejs npm
 
                                 # edit env vars
-                                echo "DB_HOST=${rds_endpoint}" | sudo tee .env 
+                                echo "DB_HOST=${local.rds_endpoint}" | sudo tee .env 
                                 echo "DB_USER=${aws_db_instance.tf_rds_instance.username}" | sudo tee -a .env
                                 sudo echo "DB_PASS=${aws_db_instance.tf_rds_instance.password}" | sudo tee -a .env
                                 echo "DB_NAME=${aws_db_instance.tf_rds_instance.db_name}" | sudo tee -a .env
